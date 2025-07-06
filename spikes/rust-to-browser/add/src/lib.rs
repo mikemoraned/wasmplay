47dc18a15328b9ad8d504994cdc48949ee83df27
Mike Moran <mike@houseofmoran.com>
2025-07-06 13:27:28 +0200

    completing "Building a Component" from https://component-model.bytecodealliance.org/language-support/rust.html#6-building-a-component

--- spikes/rust-to-browser/add/src/lib.rs
+++ spikes/rust-to-browser/add/src/lib.rs
@@ -1,14 +1,20 @@
 #[allow(warnings)]
 mod bindings;
 
-use bindings::Guest;
+// The comments that follow the `use` declaration below
+// correlate the rust module path segments with their
+// `world.wit` counterparts:
+use bindings::exports::docs::adder::add::Guest;
+//            <- items bundled with `export` keyword
+//                     <- package namespace
+//                           <- package
+//                                  <- interface name
 
 struct Component;
 
 impl Guest for Component {
-    /// Say hello!
-    fn hello_world() -> String {
-        "Hello, World!".to_string()
+    fn add(x: u32, y: u32) -> u32 {
+        x + y
     }
 }
 
